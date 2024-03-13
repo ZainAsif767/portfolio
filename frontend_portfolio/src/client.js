4f19432ecf1097042948635102b2a250fd574e33
Zain Asif <zain.asif@synergates.com>
2024-03-13 14:13:59 +0500

    Renamed .env files keys name

--- frontend_portfolio/src/client.js
+++ frontend_portfolio/src/client.js
@@ -1,12 +1,12 @@
-import sanityClient from '@sanity/client';
+import { createClient } from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
-export const client = sanityClient({
-    projectId: import.meta.env.REACT_APP_SANITY_PROJECT_ID,
+export const client = createClient({
+    projectId: import.meta.env.VITE_APP_SANITY_PROJECT_ID,
     dataset: 'production',
     apiVersion: '2022-02-01',
     useCdn: true,
-    token: import.meta.env.REACT_APP_SANITY_TOKEN,
+    token: import.meta.env.VITE_APP_SANITY_TOKEN,
 });
 
 const builder = imageUrlBuilder(client);
